b3e087209dbda46fe11d95f5bfa5c2c91de38a9c
Bianca Felipe <biancafelipedeoliveira@gmail.com>
2025-04-04 14:57:25 -0300

    Aula 3 Finalizada

---
+++ aula-3/script.js
@@ -0,0 +1,23 @@
+function jogar() {
+  rodada = 1;
+  while (rodada <= 3) {
+    escolhaJogador = prompt(
+      "Rodada " + rodada + ", escolha um vidro. (1 ao 3): "
+    );
+    pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+
+    if (escolhaJogador == pisoQuebrado) {
+      alert("Perdeu! Você escolheu o piso quebrado e caiu!");
+      rodada = 1000;
+    } else if (escolhaJogador != pisoQuebrado) {
+      alert(
+        "Você passou para a próxima fase! O piso quebrado estava na posição " +
+          pisoQuebrado
+      );
+    }
+    rodada = rodada + 1;
+  }
+  if (rodada == 4) {
+    alert("Parabéns! Você chegou ao final!");
+  }
+}
