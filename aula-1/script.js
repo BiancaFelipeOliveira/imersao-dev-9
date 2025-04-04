d72abe43b77d7ad46ec9aaffc7ffb1f6a6721922
Bianca Felipe <biancafelipedeoliveira@gmail.com>
2025-04-03 22:21:49 -0300

    Finalizei o exercício 1

---
+++ aula-1/script.js
@@ -0,0 +1,30 @@
+
+
+
+function conversordolar(){
+    const valor = (document.getElementById("valorInput").value);
+    dolar = 5.63
+    resultado = dolar * valor
+    document.getElementById("resultado").innerHTML = "O valor em reais convertido para dólar é: US$ " + resultado.toFixed(2);
+};
+
+function conversoreuro(){
+    const valor = (document.getElementById("valorInput").value);
+    euro = 6.23
+    resultado = euro * valor
+    document.getElementById("resultado").innerHTML = "O valor em reais convertido para euro é: €" + resultado.toFixed(2);
+};
+
+function conversorlibra(){
+    const valor = (document.getElementById("valorInput").value);
+    libra = 7.37
+    resultado = libra * valor
+    document.getElementById("resultado").innerHTML = "O valor em reais convertido para libra é: £ " + resultado.toFixed(2);
+};
+
+function conversorwon(){
+    const valor = (document.getElementById("valorInput").value);
+    won = 0.0039
+    resultado = won * valor
+    document.getElementById("resultado").innerHTML = "O valor em reais convertido para won é:  ₩ " + resultado.toFixed(2);
+};
